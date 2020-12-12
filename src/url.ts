95f613daae92fc4e2bb65e936f41ef7ad0f99c13
Dan Korzh <lienglex@gmail.com>
2020-12-12 13:34:08 +0200

    add: home; employees end points

---
+++ src/url.ts
@@ -0,0 +1,11 @@
+export const home: string = "http://localhost:3001";
+export const employees: string = "employees";
+
+export const resources = {
+  employees: {
+    businessAnalysts: `${home}/${employees}?position=businessAnalyst`,
+    qualityAssurance: `${home}/${employees}?position=qualityAssurance`,
+    projectManagers: `${home}/${employees}?position=projectManagers`,
+    programmers: `${home}/${employees}?position=programmers`,
+  },
+};
